e38dd811cea8fd4f46f84a4522da0dc972a0b223
stephensocialwetalk <stephen@socialwetalk.com>
2017-04-26 16:38:38 +0100

    a

--- workhardplayhard.js
+++ workhardplayhard.js
@@ -102,7 +102,7 @@ var utils = {
 
 canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
-W = canvas.width = (2800)
+W = canvas.width = (2100)
 H = canvas.height = (250);
 
 gridX = 5;
